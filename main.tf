18f97a1c178b6912fc6f5fad3f90931a65b3ae9c
shwetha-rg <shweask@gmail.com>
2025-07-29 22:13:04 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource local_file filecreation {
+filename = "class1.txt"
+content = "this is test file"
+}
+
+resource local_file filecreation2 {
+filename = "class2.txt"
+content = "this is test file"
+}
+
+resource local_file filecreation3 {
+filename = "class3.txt"
+content = "this is test file"
+}
+
